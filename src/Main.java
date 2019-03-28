0b2498e7ca8c09375a318133ae3acf3188062635
Zhenya Yurchyk <pro.scorp@gmail.com>
2019-03-29 00:54:02 +0300

    System.exit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        int x =0;
         System.out.println("Hello World!");
+        if (x==1)
+            System.exit(12);
     }
 }
